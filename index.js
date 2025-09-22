01db8f89dcfae2a60a7b953938d5e163f4901c21
Lik Hang <likhang@wittydata.com>
2025-09-22 13:56:31 +0800

    Added git ignore and tweak index

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
-
-const userRoutes = require("./routes/users");
-const bookRoutes = require("./routes/books");
-const authRoutes = require("./routes/auth");
-
+const express = require('express');
 const app = express();
-const PORT = 3000;
-
-const swaggerDocument = YAML.load("./swagger.yaml");
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use("/api/users", userRoutes);
-app.use("/api/books", bookRoutes);
-app.use("/login", authRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
 
-// Serve Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-  console.log(`📚 Swagger docs at http://localhost:${PORT}/api-docs`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
